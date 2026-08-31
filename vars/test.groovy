961ca207645b5bbc2deb27f127f285355762be43
Khan Sufiyan <168451049+Sufiyan3101@users.noreply.github.com>
2026-08-31 17:35:55 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,5 @@
-echo "This is testing the code..."
-sh "docker compose config"
-echo "Docker Compose configuration is valid..."
+def call(){
+  echo "This is testing the code..."
+  sh "docker compose config"
+  echo "Docker Compose configuration is valid..."
+}
