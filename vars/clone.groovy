799c092cc66807c907a326a28b92077681a868fa
Khan Sufiyan <168451049+Sufiyan3101@users.noreply.github.com>
2026-08-31 16:29:28 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(string url, string branch){
   echo "This is cloning the code..."
-  git clone url: "${url}", branch: "${branch}"
+  git url: "${url}", branch: "${branch}"
   echo "Cloning Successfully..."
 }
